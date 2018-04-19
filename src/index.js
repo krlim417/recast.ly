27eac2f57cb46708629d1ae9bb30a7f5fdc8183b
krlim417 <krlim417@gmail.com>
2018-04-18 18:49:21 -0700

    Remove former passed in data, and pass searchYouTube and YouTube API key

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 // TODO: Render the `App` component to the DOM
 
-ReactDOM.render(<App videos={window.exampleVideoData}/>, document.getElementById('app'));
+ReactDOM.render(<App searchYouTube={searchYouTube} API_KEY={YOUTUBE_API_KEY}/>, document.getElementById('app'));
