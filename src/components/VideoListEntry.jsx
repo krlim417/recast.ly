12a60e23b2ba54936d2a97b8b9f6d747d1c9ef58
krlim417 <krlim417@gmail.com>
2018-04-18 17:45:46 -0700

    Handle video entry title clicks

--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,10 +1,10 @@
-var VideoListEntry = ({video}) => (
+var VideoListEntry = ({handleVideoEntryTitleClick, video}) => (
   <div className="video-list-entry media">
     <div className="media-left media-middle">
       <img className="media-object" src={video.snippet.thumbnails.default.url} alt="" />
     </div>
     <div className="media-body">
-      <div className="video-list-entry-title">{video.snippet.title}</div>
+      <div className="video-list-entry-title" onClick={() =>handleVideoEntryTitleClick(video)}>{video.snippet.title}</div>
       <div className="video-list-entry-detail">{video.snippet.description}</div>
     </div>
   </div>
