982d105c81d08e69821773f5e8acff6ab54d5050
krlim417 <krlim417@gmail.com>
2018-04-18 17:02:36 -0700

    Render videos that get passed in

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,8 @@
-var VideoList = () => (
+var VideoList = ({videos}) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {videos.map(video =>
+      <VideoListEntry video={video} key={video.id.videoId}/>
+    )}
   </div>
 );
 
