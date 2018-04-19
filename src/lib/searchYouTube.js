d7835d1311ac0b6ea5da7ac048fbe59114d9ab44
krlim417 <krlim417@gmail.com>
2018-04-18 18:59:45 -0700

    Make a GET request with jQuery using input value on search bar

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,20 @@
-var searchYouTube = (options, callback) => {
-  // TODO
+var searchYouTube = ({key, query, max = 5}, callback) => {
+  $.get('https://www.googleapis.com/youtube/v3/search', {
+    key: key,
+    maxResults: max,
+    part: 'snippet',
+    q: query,
+    type: 'video',
+    videoEmbeddable: 'true'
+  }).done(({items}) => {
+    if (callback) {
+      callback(items);
+    }
+  }).fail(({responseJSON}) => {
+    responseJSON.error.errors.forEach((err) =>
+      console.error(err)
+    );
+  });
 };
 
 window.searchYouTube = searchYouTube;
